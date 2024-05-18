e78f592c5147ef47fd373a534c0c2c551827f797
Matthew <matsu@sas.upenn.edu>
2024-05-18 18:11:06 -0400

    temp

---
+++ jk.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib as mpath
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+from scipy.special import erf as erf
+from functools import partial as partial
+print("hi")
