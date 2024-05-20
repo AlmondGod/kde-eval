91772e6d3bf53eb9ef29dec07ae79d7073a142ab
Matthew <matsu@sas.upenn.edu>
2024-05-20 13:26:12 -0400

    update output

--- jk.py
+++ jk.py
@@ -8,7 +8,11 @@ import sys
 import numpy as np
 
 def gaussian_kernel(x, sigma=1.0):
-    return np.exp(-0.5 * (x / sigma)**2)
+    return np.exp(-0.5 * (x)**2)
+
+def student_kernel(x, sigma=1.0):
+    return 1/((x)**2+1)
+
 
 datasets = {
 	'covtype': [54, 581012, 10000, 2.2499],
@@ -20,20 +24,29 @@ kernel_data = np.loadtxt("data/our_data_askit.data", delimiter=',', skiprows=0)
 test_data = np.loadtxt("data/our_data_askit_query.data", delimiter=',', skiprows=0)
 print("Dimensions of kernel_data:", kernel_data.shape)
 print("Dimensions of test_data:", test_data.shape)
-first_two_rows = test_data[:100]
+first_two_rows = test_data[1000:2000]
 kernel_sums = []
+kernel_sums2 = []
 
 for row in first_two_rows:
     kernel_sum = 0
+    kernel_sum2 = 0
     for point in kernel_data:
         diff = point - row
         kernel_sum += gaussian_kernel(np.linalg.norm(diff))
+        kernel_sum2  += student_kernel(np.linalg.norm(diff))
     kernel_sum = kernel_sum/581012
+    kernel_sum2 = kernel_sum2/581012
     kernel_sums.append(kernel_sum)
+    kernel_sums2.append(kernel_sum2)
 
 output_file = "kernel_sums_output.txt"
 with open(output_file, 'a') as f:
     for kernel_sum in kernel_sums:
         f.write(str(kernel_sum) + '\n')
+output_file1 = "student_sums_output.txt"
+with open(output_file1, 'a') as f:
+    for kernel_sum2 in kernel_sums2:
+        f.write(str(kernel_sum2) + '\n')
 
 print("Kernel sums saved to:", output_file)
