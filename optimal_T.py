e9b294b8ad56b7f6c96f8fc82744f9b9e56a0903
Matthew <matsu@sas.upenn.edu>
2024-05-22 15:40:35 -0400

    temp

---
+++ optimal_T.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random_sampling as rs
+
+#Given a delta and epsilon, find the minimum T which satisfies the problem
+#constraints, which is that the estimated KDE is within $(1 + \epsilon)$-multiplicative
+#away from the true estimate with less than $(1 - \delta)$ probability
+def optimal_T(data, fun, query, delta, epsilon):
+    T_values = rs.generate_sampling_points(len(data))
+    kde_true = rs.kernel_density_true(data, query)
+    for T in T_values:
+        num_true = 0
+        num_false = 0
+        for _ in range(1000):
+            mean_kde, _ = rs.kde_random_sampling(data, fun, query, T, num_reps=1)
+            if mean_kde >= (1 - epsilon) * kde_true and mean_kde <= (1 + epsilon) * kde_true:
+                num_true += 1
+            else:
+                num_false += 1
+        if(num_false/1000 <= delta):
+            return T
+    return None
+
+#computing optimal value of T for various combinations of delta and epsilon
+#we use the same data and query point as in random_sampling.py
+
+#delta varies between 0 and 1 and measures permitted failure rate: 
+eval_deltas = [0.99, 0.9, 0.5, 0.1, 0.01, 0.001]
+
+#epsilon varies infinitely positive but we want a reasonably useful margin of error:
+eval_epsilons = [0.00001, 0.0001, 0.001, 0.01, 0.1, 0.5, 1]
+
+optimalTs_gaussian = np.zeros((len(eval_deltas), len(eval_epsilons)))
+for i, eval_delta in enumerate(eval_deltas):
+    for j, eval_epsilon in enumerate(eval_epsilons):
+        T = optimal_T(rs.kernel_data, rs.gaussian_kernel, rs.query, eval_delta, eval_epsilon)
+        optimalTs_gaussian[i, j] = T
+        print(f"delta = {eval_delta}, epsilon = {eval_epsilon}, optimal T is {T}")
+
+optimalTs_student = np.zeros((len(eval_delta), len(eval_epsilon)))
+for i, eval_delta in enumerate(eval_deltas):
+    for j, eval_epsilon in enumerate(eval_epsilons):
+        T = optimal_T(rs.kernel_data, rs.student_kernel, rs.query, eval_delta, eval_epsilon)
+        optimalTs_student[i, j] = T
+        print(f"delta = {eval_delta}, epsilon = {eval_epsilon}, optimal T is {T}")
+
+#2 heatmaps of the optimal T values, one for gaussian, one for student
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(optimalTs_gaussian, cmap='hot', interpolation='nearest')
+ax[0].set_title('Gaussian Kernel')
+ax[0].set_xlabel('Epsilon')
+ax[0].set_ylabel('Delta')
+ax[0].set_xticks(np.arange(len(eval_epsilons)))
+ax[0].set_xticklabels(eval_epsilons)
+ax[0].set_yticks(np.arange(len(eval_deltas)))
+ax[0].set_yticklabels(eval_deltas)
+
+ax[1].imshow(optimalTs_student, cmap='hot', interpolation='nearest')
+ax[1].set_title('Student Kernel')
+ax[1].set_xlabel('Epsilon')
+ax[1].set_ylabel('Delta')
+ax[1].set_xticks(np.arange(len(eval_epsilons)))
+ax[1].set_xticklabels(eval_epsilons)
+ax[1].set_yticks(np.arange(len(eval_deltas)))
+ax[1].set_yticklabels(eval_deltas)
+
+plt.show()
---
+++ random_sampling.py
@@ -0,0 +1,114 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+def gaussian_kernel(x, sigma=1.0):
+    return np.exp(-0.5 * (x)**2)
+
+def student_kernel(x, sigma=1.0):
+    return 1/((x)**2+1)
+
+kernel_data = np.loadtxt("data/covtype_normed.csv", delimiter=',', skiprows=0)[:, 1:]
+print("Dimensions of kernel_data:", kernel_data.shape)
+
+# we use a subset of kernel_data for efficiency
+# kernel_data = kernel_data[:10000]
+kernel_data = np.random.choice(range(1, len(kernel_data)), 10000, replace=False) #random sample to reduce kernel data
+print(len(kernel_data))
+query = kernel_data[0]
+
+def kernel_density_true(data, query):
+    kde = 0
+    for row in data:
+        diff = query - row
+        kde += gaussian_kernel(np.linalg.norm(diff)) / len(data)
+    return kde
+
+kde_true = kernel_density_true(kernel_data, query)
+print("True KDE:", kde_true)
+
+def kde_random_sampling(data, fun, query, num_samples, num_reps=500):
+    kde_estimates = []
+    for _ in range(num_reps):
+        samples = np.random.choice(range(0, len(data)), num_samples, replace=False)
+        kde = 0
+        for i in samples:
+            diff = query - data[i]
+            kde += fun(np.linalg.norm(diff))
+        kde_estimates.append(kde / num_samples)
+    mean_kde = np.mean(kde_estimates)
+    var_kde = np.var(kde_estimates)
+    return mean_kde, var_kde
+
+def generate_sampling_points(max_value, base=2):
+    T_values = []
+    T = 1
+    while T <= max_value:
+        T_values.append(T)
+        T *= base
+    return np.array(T_values)
+
+# Below is for plots of initial tests of various Ts on 10000 samples of kernel_data:
+
+# max_samples = 10000
+# T_values = generate_sampling_points(max_samples)
+# print("Sampling points (T values):", T_values)
+
+# grpe = []
+# strpe = []
+# gaussian_times = []
+# student_times = []
+# gaussian_variances = []
+# student_variances = []
+
+# for T in T_values:
+#     start_time = time.time()
+#     gkde, gvar = kde_random_sampling(kernel_data, gaussian_kernel, q_point, T)
+#     gaussian_times.append(time.time() - start_time)
+#     gaussian_variances.append(gvar)
+
+#     start_time = time.time()
+#     skde, svar = kde_random_sampling(kernel_data, student_kernel, q_point, T)
+#     student_times.append(time.time() - start_time)
+#     student_variances.append(svar)
+
+#     grpe.append(np.abs((gkde - kde_true) / kde_true))
+#     strpe.append(np.abs((skde - kde_true) / kde_true))
+#     print(f"T={T}, Gaussian KDE={gkde}, gRPE={grpe[-1]}, Student-t KDE={skde}, stRPE={strpe[-1]} ")
+
+# output_file = "RPE_output.txt"
+# with open(output_file, 'a') as f:
+#     for i, t in enumerate(T_values):
+#         f.write(f"At t = {t}, gaussian RPE={grpe[i]} and student RPE={strpe[i]} \n" + 
+#                 f"with gaussian time={gaussian_times[i]} and student time={student_times[i]}\n")  
+# print("RPEs saved to:", output_file)
+
+# fig, axs = plt.subplots(3, 1, figsize=(10, 15))
+
+# axs[0].plot(T_values, grpe, label='Gaussian Kernel Loss')
+# axs[0].plot(T_values, strpe, label='Student-t Kernel Loss')
+# axs[0].set_xlabel('Number of Samples (T)')
+# axs[0].set_ylabel('RPE')
+# axs[0].set_xscale('log')
+# axs[0].set_yscale('log')
+# axs[0].legend()
+# axs[0].set_title('Kernel RPE')
+
+# axs[1].plot(T_values, gaussian_times, label='Gaussian Kernel Time')
+# axs[1].plot(T_values, student_times, label='Student-t Kernel Time')
+# axs[1].set_xlabel('Number of Samples (T)')
+# axs[1].set_ylabel('Time (seconds)')
+# axs[1].set_xscale('log')
+# axs[1].legend()
+# axs[1].set_title('Kernel Computation Time')
+
+# axs[2].plot(T_values, gaussian_variances, label='Gaussian Kernel Variance')
+# axs[2].plot(T_values, student_variances, label='Student-t Kernel Variance')
+# axs[2].set_xlabel('Number of Samples (T)')
+# axs[2].set_ylabel('Variance')
+# axs[2].set_xscale('log')
+# axs[2].legend()
+# axs[2].set_title('Kernel Variance')
+
+# plt.tight_layout()
+# plt.show()
