e1e1a0e3a4d26b2fbf7a6688de8064761b892a1b
Anand Majmudar <133559429+AlmondGod@users.noreply.github.com>
2024-05-22 18:02:04 -0400

    fixed optimal T

--- random_sampling.py
+++ random_sampling.py
@@ -8,24 +8,26 @@ def gaussian_kernel(x, sigma=1.0):
 def student_kernel(x, sigma=1.0):
     return 1/((x)**2+1)
 
-kernel_data = np.loadtxt("data/covtype_normed.csv", delimiter=',', skiprows=0)[:, 1:]
-print("Dimensions of kernel_data:", kernel_data.shape)
+kernel_data = np.loadtxt("data/our_data_askit.data", delimiter=',', skiprows=0)[:, 1:]
 
 # we use a subset of kernel_data for efficiency
 # kernel_data = kernel_data[:10000]
-kernel_data = np.random.choice(range(1, len(kernel_data)), 10000, replace=False) #random sample to reduce kernel data
-print(len(kernel_data))
-query = kernel_data[0]
+indices = np.random.choice(range(1, len(kernel_data)), 10000, replace=False) #random sample to reduce kernel data
+kernel_data = kernel_data[indices]
+print("Dimensions of kernel_data:", kernel_data.shape)
+query = kernel_data[1]
+print(query)
 
-def kernel_density_true(data, query):
+def kernel_density_true(data, query, fun):
     kde = 0
     for row in data:
         diff = query - row
-        kde += gaussian_kernel(np.linalg.norm(diff)) / len(data)
+        kde += fun(np.linalg.norm(diff)) / len(data)
     return kde
 
-kde_true = kernel_density_true(kernel_data, query)
-print("True KDE:", kde_true)
+kde_true_gauss = kernel_density_true(kernel_data, query, gaussian_kernel)
+kde_true_student = kernel_density_true(kernel_data, query, student_kernel)
+print(kde_true_gauss, kde_true_student)
 
 def kde_random_sampling(data, fun, query, num_samples, num_reps=500):
     kde_estimates = []
@@ -48,7 +50,7 @@ def generate_sampling_points(max_value, base=2):
         T *= base
     return np.array(T_values)
 
-# Below is for plots of initial tests of various Ts on 10000 samples of kernel_data:
+# # Below is for plots of initial tests various Ts on 10000 samples of kernel_data:
 
 # max_samples = 10000
 # T_values = generate_sampling_points(max_samples)
@@ -63,21 +65,22 @@ def generate_sampling_points(max_value, base=2):
 
 # for T in T_values:
 #     start_time = time.time()
-#     gkde, gvar = kde_random_sampling(kernel_data, gaussian_kernel, q_point, T)
+#     gkde, gvar = kde_random_sampling(kernel_data, gaussian_kernel, query, T)
 #     gaussian_times.append(time.time() - start_time)
 #     gaussian_variances.append(gvar)
 
 #     start_time = time.time()
-#     skde, svar = kde_random_sampling(kernel_data, student_kernel, q_point, T)
+#     skde, svar = kde_random_sampling(kernel_data, student_kernel, query, T)
 #     student_times.append(time.time() - start_time)
 #     student_variances.append(svar)
 
-#     grpe.append(np.abs((gkde - kde_true) / kde_true))
-#     strpe.append(np.abs((skde - kde_true) / kde_true))
+#     grpe.append(np.abs((gkde - kde_true_gauss) / kde_true_gauss))
+#     strpe.append(np.abs((skde - kde_true_student) / kde_true_student))
 #     print(f"T={T}, Gaussian KDE={gkde}, gRPE={grpe[-1]}, Student-t KDE={skde}, stRPE={strpe[-1]} ")
 
 # output_file = "RPE_output.txt"
 # with open(output_file, 'a') as f:
+#     f.write(f"Query point: {query}\n, true Gaussian KDE={kde_true_gauss}, true Student-t KDE={kde_true_student}\n")
 #     for i, t in enumerate(T_values):
 #         f.write(f"At t = {t}, gaussian RPE={grpe[i]} and student RPE={strpe[i]} \n" + 
 #                 f"with gaussian time={gaussian_times[i]} and student time={student_times[i]}\n")  
